60ce76df54be681934615448b0e8e621c4bc0141
camaross1220 <nivanraj@micaresvc.com>
2025-09-24 19:01:54 +0800

    - setup jetpack navigation

---
+++ app/src/main/java/com/raj/tngpractice/feature/user/ui/UsersFragment.kt
@@ -0,0 +1,29 @@
+package com.raj.tngpractice.feature.user.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class UsersFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+    }
+
+}
