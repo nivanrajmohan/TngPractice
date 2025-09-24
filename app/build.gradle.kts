22f0debb366f9865e150b77483bb1757b4445c51
camaross1220 <nivanraj@micaresvc.com>
2025-09-24 18:18:22 +0800

    - added required dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,29 +1,36 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.hilt)
 }
 
 android {
     namespace = "com.raj.tngpractice"
     compileSdk = 36
-
     defaultConfig {
         applicationId = "com.raj.tngpractice"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
-
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
     buildTypes {
-        release {
+        debug {
             isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
+            isShrinkResources = false
+            buildConfigField("String", "BASE_URL", "\"https://jsonplaceholder.typicode.com/\"")
+        }
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            buildConfigField("String", "BASE_URL", "\"https://jsonplaceholder.typicode.com/\"")
         }
     }
     compileOptions {
@@ -33,16 +40,45 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+    testOptions {
+        unitTests.isIncludeAndroidResources = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.hilt.android)
+    annotationProcessor(libs.hilt.compiler)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.scalar)
+    implementation(libs.retrofit.converter.gson)
+    implementation(libs.conscrypt)
     testImplementation(libs.junit)
+    testImplementation(libs.roboelectric)
+    testImplementation(libs.mockk)
+    testImplementation(libs.truth)
+    testImplementation(libs.mock.server)
+    testImplementation(libs.turbine)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    tasks.named("bundleRelease").configure {
+        dependsOn("testDebugUnitTest")
+        dependsOn("testReleaseUnitTest")
+        dependsOn("lintVitalRelease")
+    }
+    tasks.named("assembleRelease").configure {
+        dependsOn("testDebugUnitTest")
+        dependsOn("testReleaseUnitTest")
+        dependsOn("lintVitalRelease")
+    }
 }
